be4603b86d5e57eb9f9ab205413479047c8918ff
Ksenija <ksleonteva92@gmail.com>
2022-11-10 15:36:12 +0300

    add Test_64

---
+++ Test_64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+using System;
+ 
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void PrintReqursive(int number, int currentnumber = 0)
+        {
+            if (currentnumber >= number) return;
+            Console.WriteLine(currentnumber);
+            PrintReqursive(number,currentnumber+1);
+        }
+        
+        static void Main()
+        {
+            PrintReqursive(22);
+            Console.ReadKey();
+        }
+    }
+}
